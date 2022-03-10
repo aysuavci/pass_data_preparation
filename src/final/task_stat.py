5bc4bba3913ed5ab1816c9a0b98ff2ee38828988
Melih <melih4288@gmail.com>
2022-03-10 09:00:36 +0100

    fixed the dependency problem in task_stat

--- src/final/task_stat.py
+++ src/final/task_stat.py
@@ -1,4 +1,4 @@
-r""" from pathlib import Path
+from pathlib import Path
 
 import pandas as pd
 import pytask
@@ -8,13 +8,47 @@ from src.config import BLD
 from src.config import SRC
 
 
-@pytask.mark.depends_on({
+@pytask.mark.depends_on(
+    {
         "first": BLD / "weighted_data" / "HHENDDAT_weighted.pickle",
         "second": BLD / "weighted_data" / "PENDDAT_weighted.pickle",
-    })
+    }
+)
 @pytask.mark.produces(SRC / "paper")
 def task_creating_summary_stat_tex(depends_on, produces):
-    names = ["PENDDAT", "HHENDDAT"]
+
+    df_h = pd.read_pickle(depends_on["first"])
+    df_p = pd.read_pickle(depends_on["second"])
+    with open(SRC / r"final\PENDDAT_stat.yaml") as stream:
+        dict_stat_p = yaml.safe_load(stream)
+    with open(SRC / r"final\HHENDDAT_stat.yaml") as stream:
+        dict_stat_h = yaml.safe_load(stream)
+    data = []
+    for x, _y in dict_stat_p.items():
+        data.append(df_p.reset_index()[f"{x}"])
+        df_p_stat = pd.DataFrame(data)
+        df_p_stat = df_p_stat.rename(index=dict_stat_p).T.describe()
+        df_p_stat = (
+            df_p_stat.T.drop(["25%", "75%", "count"], axis=1)
+            .rename(columns={"50%": "median"})
+            .round(2)
+        )
+    df_p_stat.to_latex(str(Path(produces)) + "/PENDDAT_sum_stat.tex")
+    data = []
+    for x, _y in dict_stat_h.items():
+        data.append(df_h.reset_index()[f"{x}"])
+        df_h_stat = pd.DataFrame(data)
+        df_h_stat = df_h_stat.rename(index=dict_stat_h).T.describe()
+        df_h_stat = (
+            df_h_stat.T.drop(["25%", "75%", "count"], axis=1)
+            .rename(columns={"50%": "median"})
+            .round(2)
+        )
+    df_h_stat.to_latex(str(Path(produces)) + "/HHENDDAT_sum_stat.tex")
+
+
+r"""     names = ["PENDDAT", "HHENDDAT"]
+
     for i in names:
         df = pd.read_pickle(str(Path(depends_on)) + f"/{i}_weighted.pickle")
 
@@ -32,4 +66,4 @@ def task_creating_summary_stat_tex(depends_on, produces):
             .round(2)
         )
         df_stat.to_latex(str(Path(produces)) + f"/{i}_sum_stat.tex")
- """
+        """
